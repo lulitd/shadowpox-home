a835a2e7440304f4f870edebe696b41445c829f8
LaLaine Ulit-Destajo <lulitdestajo@gmail.com>
2020-04-13 20:05:57 -0400

    updated end screen with new text

--- src/screens/endScreen.jsx
+++ src/screens/endScreen.jsx
@@ -23,16 +23,16 @@ class EndScreen extends Component {
       returnToGame: false,
       stayedHome: stayedHome
     };
-    this.textStayed = ["Because you chose to stay home, {{infected}} people in your community did not catch the shadowpox virus from you.", "{{hospital}} of these would have needed hospital care."];
     this.textOut = ["Because you chose not to stay home, {{infected}} people in your community caught the shadowpox virus from you.", "{{hospital}} of these needed hospital care."];
 
-    this.textZero = ["Congratulations!","Because you stayed at home, no one in your community caught the shadowpox virus from you, and no one needed hospital care."];
+    this.textZero = ["Congratulations!", "Because you stayed at home, no one in your community caught the shadowpox virus from you, and no one needed hospital care."];
 
   }
 
   render() {
     const { cards, stayedHome } = this.state;
-
+    const infected = cards?.length ?? 0;
+    
     return (
       <Flex className="App" flexDirection="column" alignItems='center' justifyContent="center" >
         <Flex flexDirection='column' width='fit-content'>
@@ -41,7 +41,7 @@ class EndScreen extends Component {
             <form encType="application/x-www-form-urlencoded" action="https://shadowpox.org/cards/" method="POST">
               <input hidden name="cards" type="text" size="32" readOnly={true} value={cards} />
               <input hidden name="stayedhome" type="checkbox" readOnly={true} value='yes' checked={stayedHome} />
-              <MenuButton my='2' width={'fit-content'} type='submit' fontSize={[1, 2, 3]}>{`View your ${stayedHome ? 'Protection' : 'Infection'} Collection`}</MenuButton>
+              <MenuButton my='2' width={'fit-content'} type='submit' fontSize={[1, 2, 3]}>{`View your ${infected==0 ? 'Protection' : 'Infection'} Collection`}</MenuButton>
             </form>
             <MenuButton width='fit-content' fontSize={[1, 2, 3]} onClick={() => { this.OnReturn() }}>Try Again</MenuButton>
           </Flex>
@@ -76,7 +76,7 @@ class EndScreen extends Component {
     const { cards, stayedHome } = this.state;
     const sick = cards?.length;
     const hospital = cards.filter(c => c < 0).length ?? 0;
-    const text = sick == 0 ? this.textZero : (stayedHome ? this.textStayed : this.textOut);
+    const text = sick == 0 ? this.textZero : this.textOut;
     return (
       <Flex flexDirection="column" alignItems="center" p={3} maxWidth='650px' textAlign='center' flex="1 1 auto" justifyContent='center'>
 
